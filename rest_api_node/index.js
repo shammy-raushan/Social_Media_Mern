831991e6050ef7d7c09cde13499ad2ec479f3abe
Shammy Raushan <shammy.raushan@freecharge.com>
2023-07-03 10:58:09 +0530

    social_media

---
+++ rest_api_node/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
+// dotenv.config();
+
+const url =
+  "mongodb+srv://shammyshweta:shammyshweta@social.x8tfctu.mongodb.net/?retryWrites=true&w=majority";
+//   "mongodb+srv://shammyraushan:shammyraushan@cluster0.rjgms47.mongodb.net/";
+
+mongoose
+  .connect(url, {
+    serverSelectionTimeoutMS: 5000,
+  })
+  .then(() => console.log("Connected Successfully to MongoDB"))
+  .catch((err) => console.log(err.reason));
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+// // middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is ready!");
+});
