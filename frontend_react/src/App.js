8ffafb89729c4a07051c5d9e17cc706db12632e9
Shammy Raushan <109515059+shammy-raushan@users.noreply.github.com>
2023-07-03 11:37:35 +0530

    Add files via upload

---
+++ frontend_react/src/App.js
@@ -0,0 +1,50 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import { BrowserRouter, Link, Route, Routes, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+function App() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={user ? <Home /> : <Register />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/register"
+            element={user ? <Navigate to="/" /> : <Register />}
+          />
+
+          <Route path="/profile/:username" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/*     
+      <Router>
+        <Routes>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/profile/:username">
+            <Profile />
+          </Route>
+        </Routes>
+      </Router> */}
+    </>
+  );
+}
+
+export default App;
